6a326ad54a9805f5d38826680c04f25f8029fc2c
Paasky <pekko.tuomisto@gmail.com>
2024-03-18 21:33:03 +0000

    Readme updates

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,8 +15,11 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('admin') }}" :active="request()->routeIs('admin')">
+                    <x-nav-link href="{{ url('/admin') }}">
                         {{ __('Admin Panel') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ url('/admin/tasks') }}">
+                        {{ __('Tasks') }}
                     </x-nav-link>
                 </div>
             </div>
